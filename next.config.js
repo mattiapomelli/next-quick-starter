7c734999539351051065b3f413e9437220afcf22
Mattia Pomelli <mattiapomelli@gmail.com>
2022-04-28 09:14:01 +0200

    Add svgr

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+    })
+
+    return config
+  },
+  ...nextConfig,
+}
